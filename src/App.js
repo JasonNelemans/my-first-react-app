ff62456321192d74fc6a67328a3bb5ee46f0e661
JasonNelemans <jasonnelemans@hotmail.com>
2020-02-24 11:45:15 +0100

    First change to App

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is me learning React.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello world
         </a>
       </header>
     </div>
